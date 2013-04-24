e56728712504d6b1b81f8bc3a7f47a2e67ed0aa8
limc <limc@smvp.cn>
2013-04-24 11:58:21 +0800

    files for packaging

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='textminer',
+    version='0.1.0',
+    author='Mengchen LEE',
+    author_email='CooledCoffee@gmail.com',
+    packages=['textminer'],
+    url='http://pypi.python.org/pypi/textminer/',
+)
