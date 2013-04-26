212361a5c3030e322ddb4a2108f1a99883b4240c
CooledCoffee <cooledcoffee@CoffeeStudio.(none)>
2013-04-26 08:43:31 +0800

    update pypi package

--- src/setup.py
+++ src/setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 
 setup(
     name='textminer',
-    version='0.1.0',
+    version='1.0.0',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
+    description='Utility for extracting values, lists and dicts from text.',
     packages=['textminer'],
-    url='http://pypi.python.org/pypi/textminer/',
+    url='https://github.com/CooledCoffee/textminer/',
 )
