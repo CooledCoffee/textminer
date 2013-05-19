2b5d962d9dc1d9e7a41aeafa151de6a8bd5f7895
CooledCoffee <limc@smvp.cn>
2013-05-20 07:58:44 +0800

    increase package version to 1.0.2

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='textminer',
-    version='1.0.1',
+    version='1.0.2',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
