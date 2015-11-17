aa306b70393cbaa38936082f3e34959f29a624f2
CooledCoffee <CooledCoffee@gmail.com>
2015-11-17 17:26:51 +0800

    use charset if provided

--- src/textminer/util.py
+++ src/textminer/util.py
@@ -54,15 +54,15 @@ def curl(url, charset=None):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/43.0.2357.81 Chrome/43.0.2357.81 Safari/537.36'}
     resp = requests.get(url, headers=headers, timeout=30)
     resp.raise_for_status()
-    if resp.headers.get('Content-Type', '').startswith('text/html'):
+    if charset is None and resp.headers.get('Content-Type', '').startswith('text/html'):
         charset = _detect_charset(resp.text[:1024])
-        if charset is not None:
-            resp.encoding = charset
+    if charset is not None:
+        resp.encoding = charset
     return compact_html(resp.text)
 
 _CHARSET_PATTERNS = [
-    re.compile('<meta http-equiv="Content-Type" content="text/html; charset=(.*)"', re.IGNORECASE),
-    re.compile('<meta charset="(.*)"', re.IGNORECASE),
+    re.compile('<meta http-equiv="content-type" content="text/html; charset=(.*)"'),
+    re.compile('<meta charset="(.*)"'),
 ]
 def _detect_charset(head):
     '''
@@ -86,6 +86,7 @@ def _detect_charset(head):
     >>> _detect_charset(html) is None
     True
     '''
+    head = head.lower()
     for pattern in _CHARSET_PATTERNS:
         match = pattern.search(head)
         if match is not None:
