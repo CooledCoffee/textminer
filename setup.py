694f80b9f567d9af1e44a6bf11c325b2b6f3a504
CooledCoffee <CooledCoffee@gmail.com>
2015-11-28 10:50:47 +0800

    version 1.2.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='textminer',
-    version='1.2.8',
+    version='1.2.9',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
