116224384eebdafead13bdd84bc5f95a64a27046
CooledCoffee <CooledCoffee@gmail.com>
2015-11-28 10:02:31 +0800

    export ExtractError

--- src/textminer/__init__.py
+++ src/textminer/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from textminer.extractors import ExtractError
 from textminer.main import compile, extract, extract_from_url
 
+ExtractError = ExtractError
 compile = compile
 extract = extract
 extract_from_url = extract_from_url
