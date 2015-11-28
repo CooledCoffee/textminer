7cf2e0b2532a3fc6388f1a7a948a414be35e1a07
CooledCoffee <CooledCoffee@gmail.com>
2015-11-28 09:58:35 +0800

    version 1.2.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='textminer',
-    version='1.2.7',
+    version='1.2.8',
     author='Mengchen LEE',
     author_email='CooledCoffee@gmail.com',
     classifiers = [
